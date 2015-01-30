dd056ea883fc4f1d7fac593d92f2cf1f12c08ef4
denisbalyko <denisbalyko@gmail.com>
2015-01-30 21:08:38 +0300

    count-neighbours.py

---
+++ count-neighbours.py
@@ -0,0 +1,20 @@
+def count_neighbours(grid, row, col):
+
+    directions = ((-1, 1), # ↖
+                 (0, 1),  # ↑
+                 (1, 1),  # ↗
+                 (-1, 0), # ←
+                 (1, 0),  # →
+                 (-1, -1),# ↙
+                 (0, -1), # ↓
+                 (1, -1)  # ↘
+                 )
+    s = 0
+    # want to go in directions / хотим пойти по направлениям
+    for direct in directions:
+        # check did not fall off the board / проверяем не упали ли с доски ?
+        if (col+direct[0] >= 0) and (col+direct[0] < len(grid[0])) and \
+           (row+direct[1] >= 0) and (row+direct[1] < len(grid)):
+            # If ok, then adds content / Если ок, добавим содержимое
+            s += grid[row+direct[1]][col+direct[0]]
+    return s
