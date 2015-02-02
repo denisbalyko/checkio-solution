0caaeb8d2b8494ee526d2dee08c076307bcd216b
denisbalyko <denisbalyko@gmail.com>
2015-02-02 20:32:14 +0300

    days-diff.py

---
+++ days-diff.py
@@ -0,0 +1,15 @@
+"""
+def days_diff(date1, date2):
+    from datetime import date
+    return (abs(date(*date1)-date(*date2))).days
+"""
+
+from datetime import date
+days_diff = lambda date1, date2: (abs(date(*date1)-date(*date2))).days
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
