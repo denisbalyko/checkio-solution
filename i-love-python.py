56d1ce652717891ae9d7df399075f66fe2e711fb
denisbalyko <denisbalyko@gmail.com>
2015-02-03 23:37:29 +0300

    i-love-python.py

---
+++ i-love-python.py
@@ -0,0 +1,9 @@
+def i_love_python():
+    """
+        He charmed me^ now in any language can not write :)
+    """
+    return "I love Python!"
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert i_love_python() == "I love Python!"
