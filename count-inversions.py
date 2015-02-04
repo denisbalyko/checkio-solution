8d203805523dbdbc4ebbcaca859b2a68650e3289
denisbalyko <denisbalyko@gmail.com>
2015-02-04 21:52:33 +0300

    count-inversions.py

---
+++ count-inversions.py
@@ -0,0 +1,17 @@
+def count_inversion(sequence):
+    flag, answer, sequence = True, 0, list(sequence)
+    while flag:
+        flag = False
+        for i in xrange(1, len(sequence)):
+            if sequence[i-1] > sequence[i]:
+                sequence[i], sequence[i-1] = sequence[i-1], sequence[i]
+                answer += 1
+                flag = True
+    return answer
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_inversion((1, 2, 5, 3, 4, 7, 6)) == 3, "Example"
+    assert count_inversion((0, 1, 2, 3)) == 0, "Sorted"
+    assert count_inversion((99, -99)) == 1, "Two numbers"
+    assert count_inversion((5, 3, 2, 1, 0)) == 10, "Reversed"
