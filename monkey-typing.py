660ce3119ccec9e49049f8bf8ab2d10044f5fa16
denisbalyko <denisbalyko@gmail.com>
2015-02-09 21:08:02 +0300

    monkey-typing.py

---
+++ monkey-typing.py
@@ -0,0 +1,17 @@
+"""
+def count_words(text, words):
+    ans = 0
+    for word in words:
+        if word in text.lower():
+            ans += 1
+    return ans
+"""
+
+count_words = lambda text, words: sum(1 for word in words if word in text.lower())
+
+if __name__ == '__main__':
+    #These uu"1sserts" using only for self-checking and not necessary for auto-testing
+    assert count_words(u"How aresjfhdskfhskd you?", {u"how", u"are", u"you", u"hello"}) == 3, "Example"
+    assert count_words(u"Bananas, give me bananas!!!", {u"banana", u"bananas"}) == 2, "BANANAS!"
+    assert count_words(u"Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+                       {u"sum", u"hamlet", u"infinity", u"anything"}) == 1, "Weird text"
