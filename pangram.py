965ade9be92195c5cadd6a234c146ab4b76c1d4d
denisbalyko <denisbalyko@gmail.com>
2015-02-12 20:14:22 +0300

    pangram.py

---
+++ pangram.py
@@ -0,0 +1,15 @@
+# why do I read? :( http://www.checkio.org/blog/pangram-review/
+from string import ascii_lowercase
+
+"""
+def check_pangram(text):
+    return set(ascii_lowercase).issubset(set(text.lower()))
+"""
+
+check_pangram = lambda t: set(ascii_lowercase).issubset(set(t.lower()))
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_pangram("The quick brown fox jumps over the lazy dog."), "brown fox"
+    assert not check_pangram("ABCDEF"), "ABC"
+    assert check_pangram("Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!"), "Bored?"
