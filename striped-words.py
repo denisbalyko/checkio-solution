ce29243d1afbe97aa2c151b33278d5ef779e0945
denisbalyko <denisbalyko@gmail.com>
2015-02-17 00:16:41 +0300

    striped-words.py typo

--- striped-words.py
+++ striped-words.py
@@ -1,29 +1,32 @@
-import string
+from string import ascii_letters, punctuation
 VOWELS = "AEIOUY"
 CONSONANTS = "BCDFGHJKLMNPQRSTVWXZ"
 
 
 def checkio(text):
-    ans, text, vowels, consonants = 0, text.lower(), VOWELS.lower(), CONSONANTS.lower()
+    ans, text = 0, text.lower()
+    vowels, consonants = VOWELS.lower(), CONSONANTS.lower()
 
-    for i in range(1,len(text)):
-        if text[i] in string.punctuation:
-            text = text.replace(text[i], " ")
+    for p in punctuation:
+        text = text.replace(p, " ")
 
     for word in text.split():
-        if len(word)>1:
-            w1 = word[::2]
-            w2 = word[1::2]
-            if set(w1).issubset(set(string.ascii_letters)) and  set(w2).issubset(set(string.ascii_letters)):
-                if set(w1).issubset(set(vowels)) and set(w2).issubset(set(consonants)):
-                    ans = ans + 1
-                if set(w2).issubset(set(vowels)) and set(w1).issubset(set(consonants)):
+        if len(word) > 1:
+            odd = word[::2]
+            even = word[1::2]
+            if set(odd).issubset(set(ascii_letters)) and \
+               set(even).issubset(set(ascii_letters)):
+                if (set(odd).issubset(set(vowels)) and
+                    set(even).issubset(set(consonants))) or \
+                   (set(even).issubset(set(vowels)) and
+                    set(odd).issubset(set(consonants))):
+                    # odd-vow even-cons OR odd-cons even-vow
                     ans = ans + 1
     return ans
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    #assert checkio(u"My name is ...") == 3, "All words are striped"
-    #assert checkio(u"Hello world") == 0, "No one"
-    #assert checkio(u"A quantity of striped words.") == 1, "Only of"
+    assert checkio(u"My name is ...") == 3, "All words are striped"
+    assert checkio(u"Hello world") == 0, "No one"
+    assert checkio(u"A quantity of striped words.") == 1, "Only of"
     assert checkio(u"Dog,cat,mouse,bird.Human.") == 3, "Dog, cat and human"
