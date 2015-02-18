248039c3d62cda326f53e68ad14970d1e58ff323
denisbalyko <denisbalyko@gmail.com>
2015-02-18 23:25:39 +0300

    bird-language.py

---
+++ bird-language.py
@@ -0,0 +1,30 @@
+VOWELS = "aeiouy"
+
+
+def translate(phrase):
+    list_vowels, i = list(VOWELS), 0
+    while i < len(phrase):
+        if phrase[i] == " ":
+            i = i + 1
+            continue
+        if phrase[i] in list_vowels:
+            try:
+                pass
+                if phrase[i] == phrase[i+1] and phrase[i+1] == phrase[i+2]:
+                    phrase = phrase[:i+1] + phrase[i+3:]
+            except IndexError, e:
+                raise e
+        else:
+            try:
+                phrase = phrase[:i+1] + phrase[i+2:]
+            except IndexError, e:
+                pass
+        i = i + 1
+    return phrase
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert translate(u"hieeelalaooo") == "hello", "Hi!"
+    assert translate(u"hoooowe yyyooouuu duoooiiine") == "how you doin", "Joey?"
+    assert translate(u"aaa bo cy da eee fe") == "a b c d e f", "Alphabet"
+    assert translate(u"sooooso aaaaaaaaa") == "sos aaa", "Mayday, mayday"
