834d7a362a33e4f7bcd3f36557c226dce505c583
denisbalyko <denisbalyko@gmail.com>
2015-02-23 22:42:48 +0300

    verify-anagrams.py

---
+++ verify-anagrams.py
@@ -0,0 +1,14 @@
+"""
+def verify_anagrams(first_word, second_word):
+    return sorted(first_word.replace(" ","").lower()) == sorted(second_word.replace(" ","").lower())
+"""
+
+func = lambda t: sorted(t.replace(" ", "").lower())
+verify_anagrams = lambda f, s: func(f) == func(s)
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(verify_anagrams(u"a", u"z"), bool), "Boolean!"
+    assert verify_anagrams(u"Programming", u"Gram Ring Mop") == True, "Gram of code"
+    assert verify_anagrams(u"Hello", u"Ole Oh") == False, "Hello! Ole Oh!"
+    assert verify_anagrams(u"Kyoto", u"Tokyo") == True, "The global warming crisis of 3002"
