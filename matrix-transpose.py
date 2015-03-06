dc59518944d2d8d4a45dde9704c338592ab990d9
denisbalyko <denisbalyko@gmail.com>
2015-03-06 16:24:58 +0300

    update matrix-transpose.py

--- matrix-transpose.py
+++ matrix-transpose.py
@@ -1,8 +1,10 @@
+# checkio = lambda d: zip(*d)
 checkio = lambda d: list(map(list, zip(*d)))
 
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 def test_function():
+    # assert isinstance(checkio([[0]]).pop(), list) is True, "Match types"
     assert checkio([[1, 2, 3],
                     [4, 5, 6],
                     [7, 8, 9]]) == [[1, 4, 7],
@@ -15,3 +17,7 @@ def test_function():
                     [7, 8, 1]]) == [[1, 8, 7, 4, 7],
                                     [4, 2, 8, 9, 8],
                                     [3, 6, 3, 6, 1]], "Rectangle matrix"
+
+
+if __name__ == '__main__':
+    test_function()
